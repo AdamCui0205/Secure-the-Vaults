637fe74616766f6f6f513a637e03bf916c7953ac
Peng Cui <pengcui@Pengs-MBP.lan>
2023-10-23 13:02:42 -0700

    Block7

---
+++ Secure-the-Vault.js
@@ -0,0 +1,8 @@
+// create a variable named user, assign its a value of str, You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+const user = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//create three variables, use different arthmetic operator(+-*/). let the result equal 10,40,and 39
+const codeI = 15 - 5;
+const codeII = 36 + 4;
+const codeIII = 13 * 3;
+//Create a dialog box display code and text
+alert(`${user} ${codeI}-${codeII}-${codeIII}`)
---
+++ index.js
@@ -0,0 +1,18 @@
+const cowCount = 50;
+const umbrelaCount =10*3
+console.log(umbrelaCount)
+const title = 'Jimmy\'s Greatest Hits';
+console.log(title);
+
+console.log(`Hello space World and set them to a variable`)
+
+const firstName = 'John';
+let lastName = 'Smith';
+const age = 54;
+lastName = 'Doe';
+alert(`My name is ${firstName} ${lastName} and I am ${age} years old.`)
+
+console.log(typeof(12+16));
+console.log(typeof('true'))
+console.log(typeof(null))
+console.log(typeof(false))
